19602c597b32796b7beef2494d13b34fe84caa48
0raffay <0,abdulraffay@gmail.com>
2023-03-08 01:10:15 +0500

    homePage

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// $(document).ready(function(){
+//     slickSliders()
+// })
+
+
+
+
+// function slickSliders(){
+
+//     $('.gallery-slider').slick({
+//         slidesToShow: 1,
+//         slidesToScroll: 1,
+//         Infinity: true,
+//         arrows: false,
+//         dots: false,
+//         autoplay: true,
+//         autoplaySpeed: 3000,
+//     })
+// }
+
