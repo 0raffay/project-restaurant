3773be0144cb9ae2064fa2338c097408808583ce
0raffay <0,abdulraffay@gmail.com>
2023-03-16 01:54:24 +0500

    All Pages Complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,3 +18,22 @@
 //     })
 // }
 
+const images = ["../images/background-1.jpg", "../images/background-2.jpg", "../images/background-3.jpg"];
+const timeInterval = 5000;
+let index = 0;
+
+function changeBackground() {
+    const main = document.querySelector(".main")
+    main.style.backgroundimage =  `url(${images[index]})`;
+    index = (index + 1) % images.length;
+    console.log("raffay")
+    console.log(main.style)
+}
+
+changeBackground(); 
+
+setInterval(changeBackground, timeInterval); 
+
+
+
+
