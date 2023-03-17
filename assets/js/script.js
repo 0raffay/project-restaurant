a0640daab5c49b6e58c3be5531101001b254a74f
0raffay <0,abdulraffay@gmail.com>
2023-03-18 00:40:41 +0500

    responsive

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,39 +1,23 @@
-// $(document).ready(function(){
-//     slickSliders()
-// })
 
 
+//navbar
+const burger = document.querySelector('.burger-icon')
+const header = document.querySelector("header")
+const navlist = document.querySelector('.nav-list')
+var nav = 0;
 
+burger.addEventListener('click', ()=>{
+    if(nav == 0){
+        navlist.style.display = "block"
+        nav = nav + 1
+    }
+    else{
+        navlist.style.display = "none"
+        nav = nav - 1
+    }
 
-// function slickSliders(){
-
-//     $('.gallery-slider').slick({
-//         slidesToShow: 1,
-//         slidesToScroll: 1,
-//         Infinity: true,
-//         arrows: false,
-//         dots: false,
-//         autoplay: true,
-//         autoplaySpeed: 3000,
-//     })
-// }
-
-const images = ["../images/background-1.jpg", "../images/background-2.jpg", "../images/background-3.jpg"];
-const timeInterval = 5000;
-let index = 0;
-
-function changeBackground() {
-    const main = document.querySelector(".main")
-    main.style.backgroundimage =  `url(${images[index]})`;
-    index = (index + 1) % images.length;
-    console.log("raffay")
-    console.log(main.style)
-}
-
-changeBackground(); 
-
-setInterval(changeBackground, timeInterval); 
-
-
-
+    header.addEventListener('click', function(){
+        navlist.style.display = "none"
+    })
+})
 
