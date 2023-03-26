8c52ca74e3df1fe05d754e3f6a36662c5da4a0f9
0raffay <0,abdulraffay@gmail.com>
2023-03-26 20:39:02 +0500

    form Validation

--- src code/assets/js/script.js
+++ src code/assets/js/script.js
@@ -21,3 +21,36 @@ burger.addEventListener('click', ()=>{
     })
 })
 
+// Form Success
+
+const feedbackForm = document.querySelector('.feedback-container form');
+const contactForm = document.querySelector('.contact-container form');
+
+function contactSuccess(event) {
+    event.preventDefault();
+    if (contactForm.checkValidity()) {
+        document.querySelector('.contact-container').style.display = "none";
+        document.getElementById('contact-success').style.display = "block";
+    } else {
+        contactForm.reportValidity();
+    }
+};
+
+function feedbackSuccess(event){
+    event.preventDefault();
+    if (feedbackForm.checkValidity()) {
+        document.querySelector('.feedback-container').style.display = "none";
+        document.getElementById('feedback-success').style.display = "block";
+    } else {
+        feedbackForm.reportValidity();
+    }
+};
+
+// for alphabet values only
+
+const input = document.querySelector('input[type="text"]');
+input.addEventListener('input', function(event) {
+    const regex = /[^a-zA-Z]/g;
+    input.value = input.value.replace(regex, '');
+});
+
